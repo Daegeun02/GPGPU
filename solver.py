4765d8a36f1b1bf9c376eb12d63b322c739c4a03
Daegeun02 <87759632+Daegeun02@users.noreply.github.com>
2022-08-03 11:12:29 +0900

    Add files via upload

--- solver.py
+++ solver.py
@@ -91,7 +91,7 @@ class LeastSquare:
 
 
 class MinimumEnergyControlSolver:
-    def __init__(self, x_des, x_0, upper_boundary, downer_boundary, lambdas, dt=0.1, step=300, learning_rate=1e-2, max_epoch=50, max_iteration=100):
+    def __init__(self, x_des, x_0, upper_boundary, downer_boundary, lambdas, dt=0.1, step=300, learning_rate=1e-3, max_epoch=50, max_iteration=100):
         ## important constants
         self.axis = 3
         self.DOF  = 6
